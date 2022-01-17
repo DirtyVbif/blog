8778bf4f79792fe966f9df77300bb8876f472661
DirtyVbif <funkybboy@mail.ru>
2022-01-17 20:08:12 +0300

    Add session and cookies facades

---
+++ app/core/Client/CookiesFacade.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Blog\Client;
+
+use Blog\Components\Singletone;
+
+class CookiesFacade
+{
+    use Singletone;
+
+    /**
+     * Get cookie by name
+     */
+    public function get(string $name)
+    {
+        return $_COOKIE[$name] ?? null;
+    }
+
+    /**
+     * Send a cookie. Same as setcookie() function. @see setcookie() documentation
+     * 
+     * @link https://php.net/manual/en/function.setcookie.php
+     */
+    public function set(string $name, $value, $expires_or_options = 0, $path = '/', $domain = '', $secure = false, $httponly = false)
+    {        
+        setcookie($name, $value, $expires_or_options, $path, $domain, $secure, $httponly);
+        return $this;
+    }
+
+    /**
+     * Unset cookie by name
+     */
+    public function unset(string $name): self
+    {
+        unset($_COOKIE[$name]);
+        setcookie($name, null, -1, '/');
+        return $this;
+    }
+
+    /**
+     * Check if specified cookie name exists or not
+     */
+    public function isset(string $name): bool
+    {
+        return !is_null($this->get($name));
+    }
+}
---
+++ app/core/Client/SessionFacade.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Blog\Client;
+
+use Blog\Components\SetterAndGetter;
+use Blog\Components\Singletone;
+
+class SessionFacade
+{
+    use Singletone,
+        SetterAndGetter;
+
+    protected const COOKIESACCEPTED = 'cookies-accepted';
+
+    /**
+     * Start new session or initialize existing one
+     */
+    public function start(): self
+    {
+        session_start();
+        return $this;
+    }
+
+    /**
+     * Store value into session container by name or names sequence
+     * 
+     * @param string $name name or names sequence of value in session container.
+     * Names sequence separeted with `/` or `.` or `\` symbols.
+     * * e.g.: `$name = 'user/token'` will store `$value` into `$_SESSION['user']['token']` container
+     * 
+     * @param mixed $value
+     * 
+     * @param bool $rewrite trigger to rewrite by given name existing value or not
+     */
+    public function set(string $name, $value, bool $rewrite = true): self
+    {
+        return $this->setByVarName('_SESSION', $value, false, $name, $rewrite);
+    }
+
+    public function push(string $name, $value): self
+    {
+        if (!$this->get($name) || !is_array($this->get($name))) {
+            $this->set($name, []);
+        }
+        $array = $this->get($name);
+        array_push($array, $value);
+        $this->set($name, $array);
+        return $this;
+    }
+
+    /**
+     * Get value from session container by name or names sequence.
+     * 
+     * @param string|null $name [optional] name or names sequence of array keys for session container.
+     * Names sequence can be separeted with `/` or `.` or `\` symbols.
+     * * e.g.: `$name = 'user/token'` will return `$_SESSION['user']['token']` value if it exists;
+     * * full session container will be returned if no key provided;
+     */
+    public function get(?string $name = null)
+    {
+        return $this->getByVarName('_SESSION', false, $name);
+    }
+
+    /**
+     * Check session container array if specified key is set or not
+     * 
+     * @param string $name name or names sequence of array keys for session container.
+     * Names sequence can be separeted with `/` or `.` or `\` symbols.
+     * * e.g.: `$name = 'user/token'` will return `$_SESSION['user']['token']` value if it exists;
+     */
+    public function isset(string $name): bool
+    {
+        return !is_null($this->get($name));
+    }
+
+    /**
+     * Unset session container value by name or name sequence.
+     * 
+     * @param string $name name or names sequence of value in session container.
+     * Names sequence separeted with `/` or `.` or `\` symbols.
+     * * e.g.: `$name = 'user/token'` will return `$_SESSION['user']['token']` value if it exists;
+     */
+    public function unset(string $name): self
+    {
+        $k = $this->parseKey($name);
+        $name = array_shift($k);
+        if (empty($k) && isset($_SESSION[$name])) {
+            unset($_SESSION[$name]);
+        } elseif (isset($_SESSION[$name])) {
+            $_SESSION[$name] = setamdv($k, $_SESSION[$name], unset: true);
+        }
+        return $this;
+    }
+
+    public function setCookiesAccepted(): self
+    {
+        $this->set(self::COOKIESACCEPTED, true);
+        return $this;
+    }
+
+    public function isCookiesAccepted(): bool
+    {
+        return $this->get(self::COOKIESACCEPTED) ?? false;
+    }
+}
