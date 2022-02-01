e5c3eed8eb78e200117fe6aee0474909a17aabb0
DirtyVbif <funkybboy@mail.ru>
2022-02-01 19:23:44 +0300

    complete js slider for skills block

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 docready(() => {
-    const cookie_agreement = new CookieAgreement;
+    const cookie_agreement = new CookieAgreement,
+        skill_slider = new SkillSlider;
+
     cookie_agreement.initialize();
+    skill_slider.initialize();
 });
