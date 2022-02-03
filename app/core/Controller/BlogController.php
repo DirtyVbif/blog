1d5c356d38af24e922518c183be3bb2cfea74498
DirtyVbif <funkybboy@mail.ru>
2022-02-03 22:32:27 +0300

    add methods for BlogController::controller to get articles

--- app/core/Controller/BlogController.php
+++ app/core/Controller/BlogController.php
@@ -16,4 +16,29 @@ class BlogController extends BaseController
     {
         return t('blog');
     }
+
+    public function getArticles(int $limit = 0, bool $order_desc = false): array
+    {
+        $sql = sql_select(
+            ['id', 'title', 'summary', 'body', 'created', 'status', 'alias', 'preview_src', 'preview_alt'],
+            'articles'
+        );
+        $sql->limit($limit);
+        $order = 'ASC';
+        if ($order_desc) {
+            $order = 'DESC';
+        }
+        $sql->order('created', $order);
+        return $sql->all();
+    }
+
+    public function getArticleById(int $id)
+    {
+        $sql = sql_select(
+            ['id', 'title', 'summary', 'body', 'created', 'status', 'alias', 'preview_src', 'preview_alt'],
+            'articles'
+        );
+        $sql->where(condition: ['id' => $id]);
+        return $sql->first();
+    }
 }
