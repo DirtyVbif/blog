a6e91e0437507b72961a219dcd6e2049052f2c45
DirtyVbif <funkybboy@mail.ru>
2022-02-06 23:49:24 +0300

    update summary content

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,26 @@ docready(() => {
 
     cookie_agreement.initialize();
     skill_slider.initialize();
+
+    const print_summary = document.getElementById('print-summary');
+    if (print_summary) {
+        print_summary.addEventListener('click', printSummary);
+    }
 });
+
+function printSummary()
+{
+    const summary = document.getElementById('summary');
+    if (!summary) {
+        return;
+    }
+    const temp = window.open('', '', 'height=600, width=600');    
+    temp.document.write('<html>');
+    temp.document.write('<head><style type="text/css" media="all">.summary-card{min-height:150px;}.summary-card img{float:left;margin-right:15px;width:150px;height:auto;max-width:40%;object-fit:contain;border-radius:4px;}</style></head>');
+    temp.document.write('<body>');
+    temp.document.write(summary.innerHTML);
+    temp.document.write('</body></html>');
+    temp.document.getElementById('print-summary').remove();
+    temp.document.close();
+    temp.print();
+}
