022b57bc28b0e60e60bba23bd82fc04930fd71d9
DirtyVbif <funkybboy@mail.ru>
2022-02-07 21:14:46 +0300

    add system development message

--- public/index.php
+++ public/index.php
@@ -5,4 +5,10 @@ define('ROOTDIR', '../');
 require_once ROOTDIR . 'app/autoload.php';
 
 app()->run();
+
+msgr()->warning(
+    'Обратите внимание: в данный момент сайт находится в процессе разработки. Если Вы наткнулись на какую-либо ошибку в работе сайта, пожалуйста, @contact_me.',
+    markup: ['contact_me' => '<a href="' . tpllink('contacts', '/')->url . '">свяжитесь со мной</a>']
+);
+
 echo app();
