bf966b6a2af7f8b97adce05d6e00f5563b810123
DirtyVbif <funkybboy@mail.ru>
2022-02-14 03:39:43 +0300

    complete Library::module usage method()

--- app/core/Modules/Library/AbstractLibrary.php
+++ app/core/Modules/Library/AbstractLibrary.php
@@ -7,10 +7,18 @@ abstract class AbstractLibrary
     protected const JS_STRICT_MODE = '"use strict";';
     
     abstract public function use(): void;
-    abstract protected function getSrcListByKey(string $source_key): array;
 
     /**
-     * Get relative path to current library directory with trailing slash.
+     * Method to get data of using js and css sources for library as object with `stack` and `public` fields.
+     * 
+     * @return object must contains `stack` and `public` fields, where:
+     * * `stack` is @var string[] with names of sources;
+     * * `public` is @var string name of public source file.
+     */
+    abstract protected function getSources(): object;
+
+    /**
+     * Get relative path to current library directory with trailing slash `/`.
      * 
      * @return string `relative/path/to/library/`
      */
@@ -21,16 +29,60 @@ abstract class AbstractLibrary
         return LIBDIR . implode('/', $dir) . '/';
     }
 
-    protected function getSrcContent(string $source_key): string
+    protected function getJsSrcContent(): string
     {
-        if (!isset($this->src_content[$source_key])) {
-            $this->src_content[$source_key] = '';
-            foreach ($this->getSrcListByKey($source_key) as $source) {
-                $this->src_content[$source_key] .= file_get_contents($this->getSelfDir() . "{$source_key}/{$source}");
+        if (!isset($this->src_content['js']) && isset($this->getSources()->js)) {
+            $this->src_content['js'] = '';
+            foreach ($this->getSources()->js['stack'] as $source) {
+                $filename = $this->getSelfDir() . strPrefix($source, '/', true);
+                $this->src_content['js'] .= file_get_contents($filename);
             }
         }
-        return $source_key === 'js' ?
-            (string)self::JS_STRICT_MODE . $this->src_content[$source_key]
-            : $this->src_content[$source_key];
+        return (string)self::JS_STRICT_MODE . $this->src_content['js'];
+    }
+
+    protected function checkPublicSources(): void
+    {
+        if (isset($this->getSources()->js) && !$this->verifyPublicJsSource()) {
+            $this->makePublicJsSource();
+        }
+        if (isset($this->getSources()->css) && !$this->verifyPublicCssSource()) {
+            $this->makePublicCssSource();
+        }
+        return;
+    }
+
+    protected function verifyPublicJsSource(): bool
+    {
+        $public_filename = $this->getSources()->js['public'];
+        $public_file = f($public_filename);
+        if (!$public_file->exists()) {
+            return false;
+        }
+        return hash_equals(
+            md5($this->getJsSrcContent()),
+            md5($public_file->realContent())
+        );
+    }
+
+    protected function makePublicJsSource(): void
+    {
+        if ($public_name = $this->getSources()?->js['public'] ?? null) {
+            f($public_name)
+                ->content($this->getJsSrcContent())
+                ->save();
+        }
+        return;
+    }
+
+    protected function verifyPublicCssSource(): bool
+    {
+        // TODO: complete verification of library public css
+        return false;
+    }
+
+    protected function makePublicCssSource(): void
+    {
+        return;
     }
 }
