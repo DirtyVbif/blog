c47804f403365f67ec9e2c4ba86d4e27938d7616
DirtyVbif <funkybboy@mail.ru>
2022-02-16 00:07:18 +0300

    add Entity/Feedback::class add View/Feedbacks::class add feedback view mode full with template and styles add feedbacks view for admins from CustomController::class

---
+++ app/core/Modules/Entity/Feedback.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace Blog\Modules\Entity;
+
+use Blog\Database\SQLSelect;
+use Blog\Modules\DateFormat\DateFormat;
+use Blog\Modules\Template\Element;
+use Blog\Modules\User\User;
+use Blog\Request\BaseRequest;
+
+class Feedback extends BaseEntity
+{
+    protected const ENTITY_TABLE = 'mailer_sended_mails';
+    protected const ENTITY_COLUMNS = ['id', 'subject', 'message', 'timestamp', 'headers', 'status'];
+    protected const VIEW_MODES = [
+        0 => self::VIEW_MODE_FULL
+    ];
+
+    public const VIEW_MODE_FULL = 'full';
+    public const SITEMAP_PRIORITY = 0.1;
+    public const SITEMAP_CHANGEFREQ = 'yearly';
+
+    protected SQLSelect $sql;
+
+    /**
+     * @param int|array $data is an id of article that must be loaded or already loaded article data.
+     * If integer id provided as `int $data` then article will be automatically loaded from storage.
+     * Else if array with article data provided as `array $data` then article wouldn't be loaded from storage and accept provided data.
+     */
+    public function __construct(
+        int|array $data,
+        string $view_mode = self::VIEW_MODE_FULL
+    ) {
+        if (is_int($data)) {
+            parent::__construct($data);
+        } else {
+            $this->data = $data;
+            $this->id = $data['id'] ?? 0;
+        }
+        $this->setViewMode($view_mode);
+    }
+
+    /**
+     * @return Element $tpl
+     */
+    public function tpl()
+    {
+        if (!isset($this->tpl)) {
+            $this->tpl = new Element;
+        }
+        return $this->tpl;
+    }
+
+    public function render()
+    {
+        $this->preprocessData();
+        $this->tpl()->setName('content/feedback--' . $this->view_mode);
+        $this->tpl()->setId('feedback-' . $this->id());
+        foreach ($this->data as $key => $value) {
+            $this->tpl()->set($key, $value);
+        }
+        if (!$this->status()) {
+            $this->tpl()->addClass('unpublished');
+        }
+        if (app()->user()->verifyAccessLevel(4)) {
+            $this->tpl()->set('admin_access', true);
+        }
+        return parent::render();
+    }
+
+    protected function preprocessData(): void
+    {
+        if (!empty($this->data)) {
+            $this->data['date'] = new DateFormat($this->data['timestamp'], DateFormat::DETAILED);
+            $this->data['headers'] = json_decode($this->data['headers'], true);
+        }
+        return;
+    }
+
+    public static function sql(): SQLSelect
+    {
+        $sql = sql_select(from: self::ENTITY_TABLE);
+        $sql->columns(self::ENTITY_COLUMNS);
+        return $sql;
+    }
+
+    public function loadById(int $id): self
+    {
+        $sql = self::sql();
+        $sql->where(['id' => $id]);
+        $this->data = $sql->first();
+        $this->loaded = true;
+        $this->exists = !empty($this->data);
+        return $this;
+    }
+
+    public function status(): bool
+    {
+        $status = (int)$this->data['status'] ?? 0;
+        return $status;
+    }
+    
+    /**
+     * @param \Blog\Request\BaseRequest $data
+     */
+    public static function create(BaseRequest $data): bool
+    {
+        // TODO: complete create method
+        return false;
+    }
+
+    /**
+     * @return Feedbacks[] $feedbacks
+     */
+    public static function loadByIds(array $ids, string $view_mode = self::VIEW_MODE_FULL): array
+    {
+        $feedbacks = [];
+        $sql = self::sql();
+        $sql->where(condition: ['id' => $ids], operator: 'IN');
+        foreach ($sql->all() as $feedback) {
+            $feedbacks[$feedback['id']] = new self($feedback, $view_mode);
+        }
+        return $feedbacks;
+    }
+
+    /**
+     * @param string $view_mode is name of view mode. Also named constants are available:
+     * * Comment::VIEW_MODE_FULL
+     */
+    public function setViewMode(string $view_mode): self
+    {
+        if (in_array($view_mode, self::VIEW_MODES)) {
+            $this->view_mode = $view_mode;
+        } else {
+            $this->view_mode = self::VIEW_MODE_FULL;
+        }
+        return $this;
+    }
+
+    public static function getSitemapPriority(): float
+    {
+        return self::SITEMAP_PRIORITY;
+    }
+
+    public static function getSitemapChangefreq(): string
+    {
+        return self::SITEMAP_CHANGEFREQ;
+    }
+}
---
+++ app/core/Modules/View/Feedbacks.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Blog\Modules\View;
+
+use Blog\Modules\Entity\Feedback;
+use Blog\Modules\TemplateFacade\Pager;
+
+class Feedbacks extends BaseView
+{
+    public const ITEMS_PER_PAGE = 100;
+
+    protected object $view;
+
+    public function view(): object
+    {
+        if (!isset($this->view)) {
+            $this->view = (object)[
+                'items' => [],
+                'pager' => null
+            ];
+            $current_page = isset($_GET['page']) ? max((int)$_GET['page'], 0) : 0;
+            $total_items = sql_select(from: 'comments')->count();
+            if ($total_items > self::ITEMS_PER_PAGE) {
+                $this->view->pager = new Pager($total_items, self::ITEMS_PER_PAGE);
+            }
+            $offset = $current_page * self::ITEMS_PER_PAGE;
+            foreach ($this->loadData(self::ITEMS_PER_PAGE, true, $offset) as $data) {
+                $feedback = new Feedback($data, Feedback::VIEW_MODE_FULL);
+                $this->view->items[] = $feedback;
+            }
+        }
+        return $this->view;
+    }
+
+    public function preview(int $limit, string $view_mode): array
+    {
+        $items = [];
+        return $items;
+    }
+
+    /**
+     * Loads comments data from storage as array
+     * 
+     * @return array of comments data with following keys:
+     * ```
+     * array('id', 'subject', 'message', 'timestamp', 'headers', 'status');
+     * ```
+     */
+    public static function loadData(int $limit = 0, bool $order_desc = false, int $offset = 0): array
+    {
+        $sql = sql_select(from: 'mailer_sended_mails');
+        $sql->columns(['id', 'subject', 'message', 'timestamp', 'headers', 'status']);
+        $sql->limit($limit);
+        if ($offset) {
+            $sql->limitOffset($offset);
+        }
+        $order = 'ASC';
+        if ($order_desc) {
+            $order = 'DESC';
+        }
+        $sql->order('timestamp', $order);
+        return $sql->all();
+    }
+}
