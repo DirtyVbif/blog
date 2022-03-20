137f022c4274a0425a9b18f9ad4a4c37d80244f2
DirtyVbif <funkybboy@mail.ru>
2022-03-20 18:35:28 +0300

    complete AdminController::class request processors for routes: - /admin/skill/create - form for creating new entity of type "skill" - /admin/skill/{id} - full view by entity id of entity of type "skill" - /admin/skill/{id}/edit - form for editing entity of type "skill" by entity id - /admin/skill/{id}/delete - deleting entity of type "skill" by entity id

--- app/core/Controller/AdminController.php
+++ app/core/Controller/AdminController.php
@@ -4,6 +4,7 @@ namespace Blog\Controller;
 
 use Blog\Client\User;
 use Blog\Modules\Entity\EntityFactory;
+use Blog\Modules\Entity\Skill;
 use Blog\Modules\Messenger\Logger;
 use Blog\Modules\TemplateFacade\Form;
 
@@ -64,37 +65,93 @@ class AdminController extends BaseController
     {
         $argument = app()->router()->arg(3);
         $result = false;
+        if ($argument === 'create') {
+            $result = $this->getRequestSkillCreate();
+        } else if (is_numeric($argument)) {
+            $action = app()->router()->arg(4) ?? 'view';
+            $method = 'get' . pascalCase('request skill' . $action);
+            $result = method_exists($this, $method) ? $this->{$method}($argument) : false;
+        }
+        if (!$result) {
+            $this->status = 404;
+        }
+        return $result;
+    }
+
+    protected function getRequestSkillFrom(): Form
+    {
         $form = new Form('skill');
         /** @var \BlogLibrary\HtmlTagsAutofill\HtmlTagsAutofill $html_tags_autofill */
         $html_tags_autofill = app()->library('html-tags-autofill');
         $html_tags_autofill->use();
-        $form_data = [
-            'type' => 'create',
-            'action' => '/admin/skill',
-            'html_tags_autofill' => $html_tags_autofill->getTemplate('form-skill--body')
-        ];
+        $form->tpl()->set('html_tags_autofill', $html_tags_autofill->getTemplate('form-skill--body'));
+        return $form;
+    }
+
+    protected function getRequestSkillCreate(): bool
+    {
+        $this->getTitle()->set('Создание нового материала типа &laquo;навык&raquo;');
+        $form = $this->getRequestSkillFrom();
+        $form->tpl()->set('type', 'create');
+        $form->tpl()->set('action', '/admin/skill');
+        app()->page()->addContent($form);
+        return true;
+    }
+
+    protected function getRequestSkillView(int $id): bool
+    {
         /** @var \Blog\Modules\Entity\Skill $skill */
-        if (is_numeric($argument) && $skill = EntityFactory::load($argument, 'skill')) {
-            $title = "Редактирование материала типа &laquo;навык&raquo; - #{$skill->id()} " . $skill->get('title');
-            $form_data['title'] = $skill->get('title');
-            $form_data['icon_src'] = $skill->get('icon_src');
-            $form_data['icon_alt'] = $skill->get('icon_alt');
-            $form_data['body'] = $skill->get('body');
-            $form_data['id'] = $skill->id();
-            $form_data['action'] = $skill->url();
-            $form_data['type'] = 'edit';
-            $result = true;
-        } else if ($argument === 'create') {
-            $title = 'Создание нового материала типа &laquo;навык&raquo;';
-            $result = true;
+        $skill = EntityFactory::load($id, 'skill');
+        if (!$skill?->exists()) {
+            return false;
         }
+        $skill->setViewMode(Skill::VIEW_MODE_FULL);
+        $this->getTitle()->set(
+            "View of entity #{$skill->id()} &laquo;" . $skill->get('title') . '&raquo;'
+        );
+        app()->page()->addContent($skill);
+        app()->page()->useCss('css/skills.min');
+        return true;
+    }
+
+    protected function getRequestSkillEdit(int $id): bool
+    {
+        /** @var \Blog\Modules\Entity\Skill $skill */
+        $skill = EntityFactory::load($id, 'skill');
+        if (!$skill?->exists()) {
+            return false;
+        }
+        $this->getTitle()->set(
+            "Редактирование материала типа &laquo;навык&raquo; - #{$skill->id()} " . $skill->get('title')
+        );
+        $form = $this->getRequestSkillFrom();
+        $form->tpl()->set('type', 'create');
+        $form->tpl()->set('action', '/admin/skill');
+        $form->tpl()->set('title', $skill->get('title'));
+        $form->tpl()->set('icon_src', $skill->get('icon_src'));
+        $form->tpl()->set('icon_alt', $skill->get('icon_alt'));
+        $form->tpl()->set('body', $skill->get('body'));
+        $form->tpl()->set('id', $skill->id());
+        $form->tpl()->set('action', $skill->url());
+        $form->tpl()->set('type', 'edit');
+        app()->page()->addContent($form);
+        return true;
+    }
+
+    protected function getRequestSkillDelete(int $id): bool
+    {
+        $result = EntityFactory::delete($id);
         if ($result) {
-            $this->getTitle()->set($title);
-            $form->tpl()->set($form_data);
-            app()->page()->addContent($form);
+            $method = 'notice';
+            $message = 'Entity #@id of type &laquo;@type&raquo; successfully deleted.';
         } else {
-            $this->status = 404;
+            $method = 'error';
+            $message = 'Entity #@id of type &laquo;@type&raquo; wasn\'t deleted.';
         }
-        return $result;
+        msgr()->{$method}(
+            t($message, ['id' => $id, 'type' => 'skill'])
+        );
+        app()->router()->redirect('<previous>');
+        return true;
     }
 }
