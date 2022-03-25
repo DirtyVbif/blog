d735a9ba5d170146f9899a2903731ac2474ae55e
DirtyVbif <funkybboy@mail.ru>
2022-03-25 03:56:43 +0300

    Blog\User::controller: - add ability to close authorized sessions from user profile page;

--- app/core/Controller/UserController.php
+++ app/core/Controller/UserController.php
@@ -2,7 +2,9 @@
 
 namespace Blog\Controller;
 
+use Blog\Client\User;
 use Blog\Request\LoginRequest;
+use Exception;
 
 class UserController extends BaseController
 {
@@ -69,14 +71,65 @@ class UserController extends BaseController
 
     public function postRequest(): void
     {
-        $login_data = new LoginRequest($_POST);
-        if ($login_data->isValid() && app()->user()->authorize($login_data)) {
+        $type = $_POST['type'] ?? null;
+        $result = false;
+        if ($type) {
+            $method = camelCase("post request {$type}");
+            try {
+                $result = $this->{$method}();
+            } catch (Exception $e) {
+                pre([
+                    'error' => "Unknown request and method " . static::class . "::{$method}()",
+                    'post-data' => $_POST,
+                    'exception' => $e
+                ], '--q');
+            }
+        }
+        app()->router()->redirect('<current>');
+        return;
+    }
+
+    public function postRequestLogin(): bool
+    {
+        $request = new LoginRequest($_POST);
+        $result = ($request->isValid() && app()->user()->authorize($request));
+        if ($result) {
             msgr()->notice(t('You have successfully authorized.'));
         } else {
             msgr()->error(t('Wrong login or password.'));
         }
-        app()->router()->redirect('<current>');
-        return;
+        return $result;
+    }
+
+    public function postRequestCloseSession(): bool
+    {
+        $result = false;
+        if (!user()->verifyAccessLevel(User::ACCESS_LEVEL_USER)) {
+            msgr()->error(t('You are not authorized for this request.'));
+            // TODO: complete 401 error page view
+            // /** @var ErrorController $conerr */
+            // $conerr = app()->controller('error');
+            // $conerr->prepare(401);
+            // exit;
+        } else if ($sesid = $_POST['sesid'] ?? false) {
+            $sql = sql_delete(from: User::SESSIONS_TABLE);
+            $sql->where(['sesid' => $sesid]);
+            $sql->andWhere(['uid' => user()->id()]);
+            $result = (bool)$sql->delete();
+        }
+        if ($result) {
+            $title = $_POST['title'];
+            msgr()->notice(
+                t(
+                    "Session &laquo;@name&raquo; successfully closed.",
+                    ['name' => $title]
+                )
+            );
+            user()->verifySession();
+        } else {
+            msgr()->error(t('There is no such session to close.'));
+        }
+        return $result;
     }
 
     public function getTitle(): ControlledPageTitle
