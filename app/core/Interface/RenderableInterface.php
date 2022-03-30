0b1b20d7b0764970b4a6237f83f7cd92e3067b63
DirtyVbif <funkybboy@mail.ru>
2022-03-30 21:53:40 +0300

    Blog\Interface: - add `Renderable::interface` to keep `Isp` from `SOLID`;

---
+++ app/core/Interface/RenderableInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Blog\Interface;
+
+use Blog\Modules\Template\Element;
+
+interface RenderableInterface
+{
+    /**
+     * Rebuild template for new render in case ob changes for rendered template
+     */
+    public function refreshRender(): void;
+
+    /**
+     * Main render method to build configured interface into render-ready template
+     */
+    public function render(): Element;
+}
--- app/core/Interface/TemplateInterface.php
+++ app/core/Interface/TemplateInterface.php
@@ -7,22 +7,36 @@ use Blog\Modules\Template\Element;
 interface TemplateInterface
 {
     /**
-     * Main render method to build configured interface into render-ready template
-     */
-    public function render(): Element;
-
-    /**
      * Get access to interface template element
      */
     public function template(): Element;
     
     /**
-     * Field method to set input element attributes
+     * Set new attribute for element
      * 
      * @param string $name attribute name
-     * @param string $value [optional] set attribute value
-     * @param null $value [optional] set attribute without value
-     * @param bool $data_attribute would attribute have auto-prefix `data-` or not
+     * @param string|null $value [optional] set HTML element attribute value or `NULL` to set attributes without value
+     * @param bool $data_attribute statement for attribute to have auto-prefix `data-`
      */
     public function setAttribute(string $name, ?string $value = null, bool $data_attribute = false): self;
+
+    /**
+     * Add class to the temlate classlist
+     */
+    public function addClass(string|array $classlist): self;
+
+    /**
+     * Add BEM-model modificator to default classes
+     * 
+     * BEM-model modificator can be provided without `_` underscore prefix. It will be added automatically.
+     * 
+     * @param string $mod to set new modificator for classlist
+     * @param null $mod to unset defined modificator
+     */
+    public function setDefaultClassMod(?string $mod): self;
+
+    /**
+     * Set element statement to use default classes
+     */
+    public function useDefaultClass(bool $use): self;
 }
