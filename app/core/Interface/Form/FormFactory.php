50a51511e7c2fb585d815a3f21a0317b6b6b5a5c
DirtyVbif <funkybboy@mail.ru>
2022-03-30 22:34:33 +0300

    Blog\Form::interface: - now `FormFactory::class` has methods for all existing forms; - update translations; - update styles;

--- app/core/Interface/Form/FormFactory.php
+++ app/core/Interface/Form/FormFactory.php
@@ -2,6 +2,13 @@
 
 namespace Blog\Interface\Form;
 
+use Blog\Modules\Entity\Article;
+use Blog\Modules\Entity\Comment;
+use Blog\Modules\Entity\Feedback;
+use Blog\Modules\Entity\Skill;
+use BlogLibrary\HtmlTagsAutofill\HtmlTagsAutofill;
+use com_exception;
+
 class FormFactory
 {
     public static function get(string $form_name): ?Form
@@ -11,34 +18,6 @@ class FormFactory
             return null;
         }
         return self::{$method}();
-    }
-
-    public static function getFeedback(): Form
-    {
-        $form = new Form('feedback');
-        $form->setMethod('post');
-        $form->useCsrf();
-        $form->setField('type', 'hidden')
-            ->setValue('feedback');
-        $form->setField('name')
-            ->setAttribute('maxlength', 60)
-            ->required()
-            ->setLabel(t('Your name:'))
-            ->inlineLabel(true);
-        $form->setField('email', 'email')
-            ->setAttribute('maxlength', 256)
-            ->required()
-            ->setLabel(t('Your e-mail:'))
-            ->inlineLabel(true)
-            ->appendDescription('Он не будет отображаться где-либо на сайте. Это только для обратной связи с Вами.');
-        $form->setField('subject', 'textarea')
-            ->setAttribute('rows', 6)
-            ->required()
-            ->setLabel(t('Message:'));
-        $form->setSubmit()
-            ->setValue(t('Send'))
-            ->clsI('btn btn_transparent');
-        return $form;
     }
 
     public static function getLogin(): Form
@@ -52,24 +31,52 @@ class FormFactory
         $form->setSection('body');
         $form->setSection('footer');
         $form->setField('mail', 'email', section: 'body')
-            ->setLabel(t('Login:'))
-            ->setAttribute('maxlength', 256)
+            ->setLabel(t('Login') . ':')
             ->inlineLabel(true)
-            ->required();
+            ->required()
+            ->setAttribute('maxlength', 256);
         $form->setField('password', 'password', section: 'body')
-            ->setLabel(t('Password:'))
-            ->setAttribute('minlength', 8)
-            ->setAttribute('maxlength', 64)
+            ->setLabel(t('Password') . ':')
             ->inlineLabel(true)
-            ->required();
+            ->required()
+            ->setAttribute('minlength', 8)
+            ->setAttribute('maxlength', 64);
         $form->setField('remember_me', 'checkbox', section: 'footer')
             ->setLabel(t('Remember me'))
-            ->setAttribute('checked')
             ->setValue(1)
-            ->setOrder(FormField::ORDER_BEFORE_IN_LABEL);
+            ->setOrder(FormField::ORDER_BEFORE_IN_LABEL)
+            ->setAttribute('checked');
         $form->setSubmit(section: 'footer')
             ->setValue(t('Sign in'))
-            ->clsI('btn btn_transparent');
+            ->addClass('btn btn_transparent');
+        return $form;
+    }
+
+    public static function getFeedback(): Form
+    {
+        return Feedback::getForm();
+    }
+
+    public static function getComment(int $entity_id, int $parent_id = 0): Form
+    {
+        return Comment::getForm($entity_id, parent_id: $parent_id);
+    }
+
+    public static function getArticle(?Article $entity = null): Form
+    {
+        $form = Article::getForm($entity);
+        $form->f('body')->appendDescription(
+            HtmlTagsAutofill::get('form-entity--body')
+        );
+        return $form;
+    }
+
+    public static function getSkill(?Skill $entity = null): Form
+    {
+        $form = Skill::getForm($entity);
+        $form->f('body')->appendDescription(
+            HtmlTagsAutofill::get('form-entity--body')
+        );
         return $form;
     }
 }
